f2d6462bd0bf092abe42401f9a69a2de9d6aca40
Felix44 <felice.dangelo2013@gmail.com>
2021-08-31 17:01:08 +0200

    renamed OPTION_* constants to COMMAND_*

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #include <zip.h>
 #include <unordered_set>
 #include "misc.cpp"
-#define OPTION_SETUP -666713357
-#define OPTION_START -1730724660
-#define OPTION_INFO -237985200
-#define OPTION_EXIT -248500869
+#define COMMAND_SETUP -666713357
+#define COMMAND_START -1730724660
+#define COMMAND_INFO -237985200
+#define COMMAND_EXIT -248500869
 
 int intaction;
 bool close = false;
@@ -34,7 +34,7 @@ int main() {
 		       "exit - close the program\n\nCurrent PK2 path: %s\n\n", path.c_str());
 		intaction = getinput();
 		switch(intaction) {
-			case OPTION_SETUP: {
+			case COMMAND_SETUP: {
 				setup:
 				printf("Insert path to your Pekka Kana 2 folder:\n\n");
 				std::string newpath;
@@ -46,17 +46,17 @@ int main() {
 				fclose(pathfile);
 				break;
 			}
-			case OPTION_START: {
+			case COMMAND_START: {
 				break;
 			}
-			case OPTION_INFO: {
+			case COMMAND_INFO: {
 				printf("PekkaZipper Version 2 Indev\n"
 				       "Created by Felix44\n"
 					   "Github: https://github.com/Felix-44/Pekka-Zipper\n\n");
 				system("pause");
 				break;
 			}
-			case OPTION_EXIT: {
+			case COMMAND_EXIT: {
 				close = true;
 				break;
 			}
