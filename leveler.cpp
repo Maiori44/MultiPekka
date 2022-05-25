9c5a26d178987b1190e049051af09751baba1d17
Felix44 <felice.dangelo2013@gmail.com>
2022-05-25 23:44:25 +0200

    read .map files' names

--- leveler.cpp
+++ leveler.cpp
@@ -1,13 +1,25 @@
-std::vec<std::string> mapfiles;
+std::vector<std::string> mapfiles;
 
 void startleveler() {
-    mapfiles.clear();
-    vectorpos = 0;
-    std::string episodepath;
-    {
-        std::string episodename = getfullinput("Insert episode name:");
-        pkepisodepath.append(path);
-		pkepisodepath.append("/episodes/");
-		pkepisodepath.append(episodename);
-    }
+	mapfiles.clear();
+	vectorpos = 0;
+	std::string episodepath;
+	{
+		std::string episodename = getfullinput("Insert episode name:");
+		episodepath.append(path);
+		episodepath.append("/episodes/");
+		episodepath.append(episodename);
+	}
+	{
+		consolelog("Locating all map files in the episode %s...", episodepath.c_str());
+		DIR *episodedir = openpkdir(episodepath.c_str());
+		struct dirent *mapentry;
+		while ((mapentry = readdir(episodedir))) {
+			std::string mapfilename = mapentry->d_name;
+			if (mapfilename.find(".map") != mapfilename.npos) {
+				mapfiles.insert(mapfiles.end(), mapfilename);
+				consolelog("Found map %s\n", mapfilename.c_str());
+			}
+		}
+	}
 }
